43a9a18ace29ca467581f7d49523d54feaa548e2
Yong <dev.yong842@gmail.com>
2015-05-12 06:31:42 -0400

    f

--- ng/buffet/controller/buffetMenuController.js
+++ ng/buffet/controller/buffetMenuController.js
@@ -5,8 +5,8 @@
     angular.module('buffetModule').controller('BuffetMenuController', 
     		function ($http, $scope, $location, $routeParams, webId) {
     	
-//    	$scope.data = webId.loadWebData($routeParams.homepage);
-    	$scope.data = webId.getWeb();
+    	$scope.data = webId.loadWebData($routeParams.homepage);
+//    	$scope.data = webId.getWeb();
 //    	$scope.$on('service.webId:updated', function(event, data, domain) {
 //    		console.log("service web id updated");
 //    		$scope.data = data;
